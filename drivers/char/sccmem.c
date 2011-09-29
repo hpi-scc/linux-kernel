ff5812fe56499a7f3bf3049f7ecefa9b5bdfa326
Jan-Arne Sobania <jan-arne.sobania@hpi.uni-potsdam.de>
2011-09-29 10:27:25 +0200

    Add latest sccmem patch

--- drivers/char/sccmem.c
+++ drivers/char/sccmem.c
@@ -10,6 +10,7 @@
  *    Jan-11-1998, C. Scott Ananian <cananian@alumni.princeton.edu>
  *  Shared /dev/zero mmaping support, Feb 2000, Kanoj Sarcar <kanoj@sgi.com>
  */
+
 #include <linux/version.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -18,25 +19,26 @@
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
 #include <linux/mm.h>
-#include <linux/sched.h>	/* struct task_struct */
-#include <asm/boot.h>		/* LOAD_PHYSICAL_ADDR */
 #include <linux/miscdevice.h>
 #include <linux/highmem.h>
+
 #ifdef MODVERSIONS
 #  include <linux/modversions.h>
 #endif
 #include <asm/io.h>
 
-#include <linux/kernel_stat.h>
-#include <linux/time.h>
-#include <asm/cputime.h>
 #include <linux/sccsys.h>
-
 
 /* DEBUG messages */
 #define DEBUG_MSG 0
 #define MAJOR_ADDR 222
 #define PRINTD(format, args...) if (DEBUG_MSG) { printk(format, ##args); }
+
+/* Symbols */
+#define OWN_MPB     0xd8000000
+
+#define MPBADDRBITS 13
+#define MPBSIZE     (1<<MPBADDRBITS)
 
 /* PSE bit for Pentium+ equals MPE (message buffer enable) flag in SCC! So, use it to create _PAGE_MPB symbol... */
 #define _PAGE_MPE _PAGE_PSE
@@ -46,11 +48,20 @@
  * 2048 sets, i.e. the distances between addresses in a pair of ways is
  * 2048*32B = 64kB.
  */
-#define L2_LINESIZE 32
-#define L2_WAYS     4
-#define L2_CAPACITY (256*1024)
+#define L2_LINESIZE 32UL
+#define L2_WAYS     4UL
+#define L2_CAPACITY (256*1024UL)
 #define L2_WBSTRIDE (L2_CAPACITY/L2_WAYS)
 
+/* Enable this to have the L2 flush routine execute a 'wbinvd'
+ * instruction before doing the actual flush. */
+// #define FLUSH_USES_WBINVD
+
+
+/* Virtual address and current offset in our flush-data block,
+ * which will be in an unused MPB address range */
+static size_t OWN_MPB_vaddr = 0;
+static size_t OWN_MPB_offset = 0;
 
 
 /* Methods of the character device */
@@ -176,10 +187,10 @@ static ssize_t sccdcm_read(struct file *filp,
   struct task_struct  *tsk;
   struct mm_struct    *mm;
   
-	pgd_t *pgd;
-	pud_t *pud;
-	pmd_t *pmd;
-	pte_t *pte;
+  pgd_t *pgd;
+  pud_t *pud;
+  pmd_t *pmd;
+  pte_t *pte;
   
   unsigned long physAddress;
   unsigned long address = (unsigned long)buf;
@@ -196,7 +207,8 @@ static ssize_t sccdcm_read(struct file *filp,
   pmd = pmd_offset(pud, address);
   if (pmd_none(*pmd) ) return -EADDRNOTAVAIL;
   pte = pte_offset_map(pmd, address);
-	if (pte_none(*pte) )
+
+  if (pte_none(*pte) )
   {
     pte_unmap(pte);
     return -EADDRNOTAVAIL;
@@ -212,34 +224,21 @@ static ssize_t sccdcm_read(struct file *filp,
 }
 
 
-/* Helper function to purge a user-space physical address from the caches
- * by reading kernel data into all ways
+/* Helper function to purge a specific set from the caches
+ * by reading invalid data into all ways
  */
-inline void sccdcm_purgeAddress(unsigned long physAddress)
+__attribute__((always_inline)) static inline void sccdcm_purgeSet(unsigned long set)
 {
-  register char            tmp;
-  register volatile char*  dummyData;
-  
-  /* Determine in which set the address would be stored by stripping of the
-   * MSBs, i.e. when adding the stride distance we would hit the same index.
-   */
-  const size_t set = physAddress % L2_WBSTRIDE;
-  
+  register char   tmp;
+
   /* Translate the set to a kernel space virtual address */
-  dummyData = (volatile char*)(__va(LOAD_PHYSICAL_ADDR+set));
+  const volatile char*  dummyData = (volatile char*)set;
   
   /* Now read new data into all four ways */
   tmp = *dummyData;
   tmp = *(dummyData + L2_WBSTRIDE);
   tmp = *(dummyData + L2_WBSTRIDE * 2);
   tmp = *(dummyData + L2_WBSTRIDE * 3);
-
-  /* Since the cache uses an approximation to determine the least recently
-   * used way (pseudo-LRU) it is necessary to re-read the first address to
-   * be sure that all ways were touched.
-   * Note that this is only true in case of 4 ways!
-   */
-  tmp = *dummyData;
 }
 
 
@@ -254,12 +253,13 @@ static ssize_t sccdcm_write(struct file *filp,
   struct task_struct  *tsk;
   struct mm_struct    *mm;
 
-        pgd_t *pgd;
-        pud_t *pud;
-        pmd_t *pmd;
-        pte_t *pte;
+  pgd_t *pgd;
+  pud_t *pud;
+  pmd_t *pmd;
+  pte_t *pte;
 
   size_t        pos         = 0;
+  size_t        purgebase   = 0;
   unsigned long physAddress = 0;
   unsigned long address     = (unsigned long)buf;
   unsigned long flags;
@@ -268,26 +268,34 @@ static ssize_t sccdcm_write(struct file *filp,
   PRINTD(KERN_INFO "sccdcm_write(): Flushing %d bytes from 0x%08lX\n",
                     count, address);
 
-
   /* First disable interrupts to be sure nobody else accesses the cache
    * in parallel and messes up the LRU state bits.
    */
   local_irq_save(flags);
 
+  /* Flip-flop between two parts of the flush area */
+  if(OWN_MPB_offset == 0)
+    OWN_MPB_offset = L2_CAPACITY;
+  else
+    OWN_MPB_offset = 0;
 
+  /* Set basepointer to flush area we will use in this flush */
+  purgebase = OWN_MPB_vaddr + OWN_MPB_offset;
+
+#ifdef FLUSH_USES_WBINVD
   /* Now write-back and invalidate the L1 content so subsequent reads
    * cannot trigger evictions which could again mess up L2 LRU state.
-   */
-  __asm__ volatile ( "wbinvd;\t\n" );
-
+   * It is not clear that this is required - disabling it saves about 10K cycles */
+  __asm__ volatile ( "wbinvd;\n\t" );
+#endif
 
   /* In order to improve performance, check whether the entire cache has to
    * be flushed first; use count=0 as shortcut.
    */
   if ((count==0) || (count>=L2_WBSTRIDE))
   {
-    for (physAddress=0; physAddress<L2_WBSTRIDE; physAddress+=L2_LINESIZE)
-      sccdcm_purgeAddress(physAddress);
+    for (physAddress=purgebase; physAddress<purgebase+L2_WBSTRIDE; physAddress+=L2_LINESIZE)
+      sccdcm_purgeSet(physAddress);
   }
   else
   {
@@ -338,7 +346,7 @@ static ssize_t sccdcm_write(struct file *filp,
       physAddress &= PAGE_MASK;
       physAddress += ((address+pos) & ~PAGE_MASK);
 
-      sccdcm_purgeAddress(physAddress);
+      sccdcm_purgeSet((physAddress % L2_WBSTRIDE) + purgebase);
 
       /* We only have to read once per cacheline */
       pos += L2_LINESIZE;
@@ -404,8 +412,20 @@ static int __init sccmem_init(void) {
     goto out_dereg_mpb;
   }
 
+  /* Make a memory mapping for invalid MPB data to flush the L2 cache with,
+     this region starts at one L2 size beyond the MPB */
+  OWN_MPB_vaddr = (size_t) ioremap_prot(OWN_MPB + L2_CAPACITY, L2_CAPACITY * 2, 0);
+  OWN_MPB_offset = 0;
+
+  if(!OWN_MPB_vaddr)
+    goto out_dereg_dcm;
+
+  printk(KERN_INFO "sccmem_init(): Mapped MPB at %x for flushing to %x\n", (OWN_MPB + MPBSIZE), OWN_MPB_vaddr);
+
   return 0;
 
+out_dereg_dcm:
+  misc_deregister(&sccdcm_dev);
 out_dereg_mpb:
   misc_deregister(&sccmpb_dev);
 out_dereg_ncm:
@@ -423,18 +443,36 @@ static void __exit sccmem_exit(void)
   misc_deregister(&sccncm_dev);
   misc_deregister(&sccmpb_dev);
   misc_deregister(&sccdcm_dev);
+
+  /* Unmap invalid MPB memory used by L2 flush */
+  iounmap((void*)OWN_MPB_vaddr);
 }
 
 /* Perform write-back/invalidate for all cachelines holding the specified pfn */
 void scc_cop_wbinv_pfn(pfn_t pfn)
 {
-	/* BUGBUG: Yeah, I know that this is horrible... */
+	unsigned long purgebase, physAddress, offset;
+	unsigned long flags;
 
-	unsigned offset;
+	/* First disable interrupts to be sure nobody else accesses the cache
+	 * in parallel and messes up the LRU state bits. */
+	local_irq_save(flags);
 
+	/* Flip-flop between two parts of the flush area */
+	if (OWN_MPB_offset == 0) {
+		OWN_MPB_offset = L2_CAPACITY;
+	} else {
+		OWN_MPB_offset = 0;
+	}
+
+	/* Set basepointer to flush area we will use in this flush */
+	purgebase = OWN_MPB_vaddr + OWN_MPB_offset;
+
+#ifdef FLUSH_USES_WBINVD
 	/* Begin by flushing the entire L1. We don't want any stale data in it,
 	 * as the reads issued by sccdcm_purgeAddress need to go to L2. */
-	__asm__ __volatile__ ("\twbinvd\n");
+	__asm__ __volatile__ ("wbinvd;\n\t");
+#endif
 
 	/* Now flush the entire pfn out of the cache. It is no longer in L1 by
 	 * definition, so all we need to do now is kick it out of L2...
@@ -442,9 +480,14 @@ void scc_cop_wbinv_pfn(pfn_t pfn)
 	 * page afterwards or cachelines seem to reappear from nowhere. The
 	 * safest way is to unmap all page table entries referring to the page.
 	 * Alternatively, just disable interrupts around the current method. */
+	physAddress = pfn << PAGE_SHIFT;
+
 	for (offset = 0; offset < (1 << PAGE_SHIFT); offset += L2_LINESIZE) {
-		sccdcm_purgeAddress((pfn << PAGE_SHIFT) + offset);
+		sccdcm_purgeSet(purgebase + ((physAddress + offset) % L2_WBSTRIDE));
 	}
+
+	/* Re-enable interrupts */
+	local_irq_restore(flags);
 }
 EXPORT_SYMBOL(scc_cop_wbinv_pfn);
 
